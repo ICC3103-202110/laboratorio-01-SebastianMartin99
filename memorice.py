c238eca2b15b90b575eb62ee475628c7db56b4fc
Sebastian Martin Muñoz <sebamm1999@gmail.com>
2021-03-26 13:20:21 -0300

    Primer Avance

--- memorice.py
+++ memorice.py
@@ -1,1 +1,21 @@
-print ("probando")
+from numpy import random
+N_pares = int(input("Insert amount of cards to play:"))+1
+
+Cartas = []
+for i in range(N_pares):
+    Cartas.append(range(N_pares)[i])
+    Cartas.append(range(N_pares)[i])
+
+Cartas.pop(0)
+Cartas.pop(0)
+Cartas_totales = len(Cartas)
+#print (Cartas)
+Mezcla=[]
+while len(Cartas) > 0:
+    x=Cartas[random.randint(len(Cartas))]
+    Mezcla.append(x)
+    Cartas.remove(x)
+
+#print (Mezcla)
+print(Cartas_totales)
+    
